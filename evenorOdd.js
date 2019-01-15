deba3ebcb2a02c7ec5442696361ff5f6bd49f0ba
James Bell <Noxachron@gmail.com>
2019-01-15 11:03:54 -0500

    even or odd

---
+++ evenorOdd.js
@@ -0,0 +1,11 @@
+function isEven(value){
+    if (value % 2 == 0){
+      return true;
+    }
+    else
+      return false;
+  }
+/* 
+takes an input number performs 1 operation so O(1) constant time because no matter how big the number is only 1 operation is made.
+best case is the same as the worst case
+*/
